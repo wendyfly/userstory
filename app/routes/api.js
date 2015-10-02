c7bf28eb893dadecccfb2d8db65438ceaf316e56
wendyfly <wendyliao01@gmail.com>
2015-10-01 23:19:58 -0700

    wrap up api code and add first front end file

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,22 +1,34 @@
 var User = require('../models/user');  // we will manipulate User, so we need to import
 
+var Story = require('../models/story');
+
 var config = require('../../config');  // we need config file, because we need secrekey to later login method
 
-var secreKey = config.secretKey;
+var secretKey = config.secretKey;
 
+var jsonwebtoken = require('jsonwebtoken');
 
-
+function createToken(user) {
+   var token = jsonwebtoken.sign({
+        id:user._id,
+        name: user.name,
+        username: user.username
+    }, secretKey, {
+        expiresInMinute : 1440
+    });
+    return token;
+}
 
 module.exports = function(app, express) {
-    var api = express(); // inorder to have api, we need to call express.router firstly
+    var api = express.Router(); // inorder to have api, we need to call express.router firstly
     api.post('/signup', function(req, res){
         var user = new User({
-            name: req.params.name, // body is body-parse,
-            username: req.params.username,
-            password: req.params.password
+            name: req.body.name, // body is body-parse,
+            username: req.body.username,
+            password: req.body.password
         });
-        console.log("body" + req.params);
-        console.log("name" + req.params.name);
+        console.log("body is " + req.body);
+        console.log("name is " + req.body.name);
 
         user.save(function(err) {
             if(err) {
@@ -27,7 +39,6 @@ module.exports = function(app, express) {
             res.json({message: 'user has been created'});
         });
     });
-
     api.get('/users', function(req, res) {
         User.find({}, function(err, users){
             if(err) {
@@ -36,6 +47,88 @@ module.exports = function(app, express) {
             }
             res.json(users);
         })
+    })
+
+    api.post('/login', function(req, res) {
+            User.findOne({    // find specific user object
+                username: req.body.username
+            }).select('password').exec(function(err, user) {
+                if(err) throw err;
+                if(!user) {
+                    res.send({message: "User doesn't exist"})
+                } else if (user) {
+                    var validPassword = user.comparePassword(req.body.password);
+                    if(!validPassword) {
+                        res.send({message: "Invalid password"});
+                    } else {
+                        // create token
+                        var token = createToken(user);
+                        res.json({
+                            success: true,
+                            message: "successfully login ",
+                            token: token
+                        });
+                    }
+                }
+            });
+    });
+
+    // create a middleware to check whether token is valid
+    api.use(function(req, res, next){
+        console.log("some body just came to our app");
+        var token = req.body.token || req.param('token') || req.headers['x-access-token'] // three ways to get a token. It depends on where u store the token
+        // check if token exist
+        if(token) {
+            jsonwebtoken.verify(token, secretKey, function(err, decode){
+                if(err) {
+                    res.status(403).send({success: false, message: "Failed to authenticate user"})
+                } else {
+                    req.decoded = decode;
+
+                    next();
+                }
+            })
+        } else {
+            res.status(403).send({success: false, message: "No token provided"})
+        }
+
+    })
+
+    // Destination B // provide a legitimate token// direct u to homepage
+
+    /*api.get('/', function(req, res) {
+        res.json("Hello world");
+    });*/
+
+    api.route('/')
+        . post(function(req, res) {
+            var story = new Story({
+                creator: req.decoded.id,
+                content: req.body.content,
+
+            });
+            story.save(function(err) {
+                if(err) {
+                    res.send(err);
+                    return;
+                }
+                res.json({message: "New Story Created"});
+            })
+        })
+
+        .get(function(req, res) {
+            Story.find({creator: req.decoded.id}, function(err, stories) {
+                if(err) {
+                    res.send(err);
+                    return;
+                }
+                res.json(stories);
+            })
+        })
+
+
+    api.get('/me', function(req, res) {
+        res.json(req.decoded); // when we want to get decode, we cannot use middleware,so we use this to fetch login user data
     })
 
     return api;
--- server.js
+++ server.js
@@ -22,128 +22,13 @@ app.use(bodyParser.urlencoded({ extended : true })); // if we give it as false, 
 app.use(bodyParser.json());
 app.use(morgan('dev')); // logs in terminal
 
-function createToken(user) {
-   var token = jsonwebtoken.sign({
-        id:user._id,
-        name: user.name,
-        username: user.username
-    }, secretKey, {
-        expiresInMinute : 1440
-    });
-    return token;
-}
+app.use(express.static(__dirname + '/public')); // any file (css, html, js) will be render under public folder
 
-app.post('/api/signup', function(req, res){
-        var user = new User({
-            name: req.body.name, // body is body-parse,
-            username: req.body.username,
-            password: req.body.password
-        });
-        console.log("body is " + req.body);
-        console.log("name is " + req.body.name);
-
-        user.save(function(err) {
-            if(err) {
-                res.send(err);
-                return;
-            }
-
-            res.json({message: 'user has been created'});
-        });
-});
-
-app.get('/api/users', function(req, res) {
-        User.find({}, function(err, users){
-            if(err) {
-                res.send(err);
-                return;
-            }
-            res.json(users);
-        })
-    })
-
-app.post('/api/login', function(req, res) {
-        User.findOne({    // find specific user object
-            username: req.body.username
-        }).select('password').exec(function(err, user) {
-            if(err) throw err;
-            if(!user) {
-                res.send({message: "User doesn't exist"})
-            } else if (user) {
-                var validPassword = user.comparePassword(req.body.password);
-                if(!validPassword) {
-                    res.send({message: "Invalid password"});
-                } else {
-                    //// create token
-                    var token = createToken(user);
-                    res.json({
-                        success: true,
-                        message: "successfully login ",
-                        token: token
-                    });
-                }
-            }
-        });
-});
-
-// create a middleware to check whether token is valid
-app.use(function(req, res, next){
-    console.log("some body just came to our app");
-    var token = req.body.token || req.param('token') || req.headers['x-access-token'] // three ways to get a token. It depends on where u store the token
-    // check if token exist
-    if(token) {
-        jsonwebtoken.verify(token, secretKey, function(err, decode){
-            if(err) {
-                res.status(403).send({success: false, message: "Failed to authenticate user"})
-            } else {
-                req.decoded = decode;
-
-                next();
-            }
-        })
-    } else {
-        res.status(403).send({success: false, message: "No token provided"})
-    }
-
-})
-
-// Destination B // provide a legitimate token// direct u to homepage
-
-/*app.get('/', function(req, res) {
-    res.json("Hello world");
-});*/
-
-app.route('/')
-    . post(function(req, res) {
-        var story = new Story({
-            creator: req.decoded.id,
-            content: req.body.content,
-
-        });
-        story.save(function(err) {
-            if(err) {
-                res.send(err);
-                return;
-            }
-            res.json({message: "New Story Created"});
-        })
-    })
-
-    .get(function(req, res) {
-        Story.find({creator: req.decoded.id}, function(err, stories) {
-            if(err) {
-                res.send(err);
-                return;
-            }
-            res.json(stories);
-        })
-    })
-//var api = require('./app/routes/api')(app, express); // we need to put app and express here because we use app, express when export api or they will be treated as local variable
-//app.use('/api', api); // '/api' is the prefix of api, use to test
-
+var api = require('./app/routes/api')(app, express); // we need to put app and express here because we use app, express when export api or they will be treated as local variable
+app.use('/api', api); // '/api' is the prefix of api, use to test
 
 app.get('*', function(req, res) { // * means any route you go to, response this file
-    res.sendFile(__dirname + '/public/view/index.html');
+    res.sendFile(__dirname + '/public/app/view/index.html');
 });
 
 app.listen(config.port, function (err) {
